4709e47cff189087cf0aa4cdeb269bd4888591fe
Doug Bostick <dougbostick@Dougs-MacBook.local>
2022-03-07 14:30:34 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/counters_db"
+);
+
+const Counters = sequelize.define("counters", {
+  value: Sequelize.INTEGER,
+});
+
+//express
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/counters", async (req, res, next) => {
+  try {
+    const counters = await Counters.findAll();
+    res.send(counters);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post("/api/counters", async (req, res, next) => {
+  try {
+    const newnumber = Math.floor(Math.random() * 10);
+    const created = await Counters.create({ value: newnumber });
+    res.status(204).send(created);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+const init = async () => {
+  await sequelize.sync({ force: true });
+  console.log("syncd");
+  const first = Counters.create({
+    value: 1,
+  });
+  const second = Counters.create({
+    value: 2,
+  });
+  console.log("seeded");
+  const port = 9999;
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+};
+
+init();
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counters: [],
+    };
+    this.getAndRender = this.getAndRender.bind(this);
+    this.addCounter = this.addCounter.bind(this);
+  }
+  async componentDidMount() {
+    await this.getAndRender();
+  }
+  async addCounter() {
+    await axios.post("/api/counters");
+    await this.getAndRender();
+  }
+  async getAndRender() {
+    const response = await axios.get("/api/counters");
+    const data = response.data;
+    this.setState({ counters: data });
+  }
+  render() {
+    const counterEls = this.state.counters.map((counter) => {
+      return <li key={counter.id}>{counter.value}</li>;
+    });
+    return (
+      <div>
+        <h1>Counters</h1>
+        <ul>{counterEls}</ul>
+        <button onClick={this.addCounter}>Add New Value</button>
+      </div>
+    );
+  }
+}
+
+const root = document.querySelector("#root");
+ReactDOM.render(<App />, root);
